af5491466ef7a4fc91525fa1a1e32c93117fa4b3
vanmathi1998 <49603302+vanmathi1998@users.noreply.github.com>
2019-04-23 22:25:17 +0530

    3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+a=(input("enter the letter:"))
+if(a=='a' or a=="A"   or a=="e" or a=="E" or a=='i' or a=="I" or a== 'o' or a=="O" or a=='u' or a=="U" ):
+print("the given letter is vowel")
+else:
+print("the given letter is consonant")
