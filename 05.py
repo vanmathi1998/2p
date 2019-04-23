84a2ab66eadd7ff809db8880acf724c54eca9201
vanmathi1998 <49603302+vanmathi1998@users.noreply.github.com>
2019-04-23 23:16:09 +0530

    05.py

---
+++ 05.py
@@ -0,0 +1,9 @@
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+num3=int(input("enter the last number:"))
+if((num1>num2)and(num1>num3)):
+print("num1 is the largest.")
+elif((num2>num1)and(num2>num3)):
+print("num2 is the largest")
+else:
+print("num3 is the largest")
